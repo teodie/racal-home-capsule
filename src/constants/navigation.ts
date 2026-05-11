a9317eb205af51dbc9528aff2acb62e409a81847
teodie <54758584+teodie@users.noreply.github.com>
2026-05-11 17:24:26 +0800

    feat(footer): Added footer page

---
+++ src/constants/navigation.ts
@@ -0,0 +1,5 @@
+export const navigation = [
+{title: "Features", href: "#features"},
+{title: "Visual Tour", href: "#visualtour"},
+{title: "Contact", href: "#contact"},
+]
--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import Image from "next/image"
+import { logo } from "../../public/assets/assets"
+import { navigation } from "@/constants/navigation"
 
 const Footer = () => {
   return (
-    <div className='py-20'>Footer</div>
+    <div className='py-20 max-w-7xl m-auto'>
+      <div className="flex flex-col items-center border-b border-black gap-3">
+        <Image src={logo.primary} alt="Racal Home capsule logo" />
+        <h3>racalhomecapsule@gmail.com</h3>
+      </div>
+      <div className="flex justify-between py-5">
+        <h3>© 2026 Racal Home Capsule. All rights reserved.
+        </h3>
+
+        <ul className=' flex gap-4'>
+          {
+            navigation.map((nav) => (
+              <li><a href={nav.href}>{nav.title}</a></li>
+            ))
+          }
+        </ul>
+
+      </div>
+    </div>
   )
 }
 
