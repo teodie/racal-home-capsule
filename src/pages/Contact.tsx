4a0653b5014eb15a9ce79e41dcf1331c062c2f06
teodie <54758584+teodie@users.noreply.github.com>
2026-05-23 16:17:26 +0800

    feature: replaced form into a map

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,5 @@
 import ContactCard from '@/components/ContactCard'
+import GoogleMap from '@/components/GoogleMap'
 import PageTitle from '@/components/PageTitle'
 import { PhoneCallIcon, MailsIcon, MapPinned } from 'lucide-react'
 
@@ -27,31 +28,7 @@ const Contact = () => {
 
         </div>
 
-        <form action="" >
-          <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6'>
-            <div className='sm:col-span-full'>
-              <label htmlFor="name" className="block text-sm/6 font-semibold ">Name</label>
-              <div className='mt-2'>
-                <input id='name' type="text" placeholder='juan Dela Cruz' className="block w-full rounded-md bg-amber-600/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-black/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-600 sm:text-sm/6 " />
-              </div>
-            </div>
-
-            <div className='sm:col-span-full'>
-              <label htmlFor="email" className="block text-sm/6 font-semibold ">Email</label>
-              <div className='mt-2'>
-                <input id='name' type="text" placeholder='juandelacruz@gmail.com' className="block w-full rounded-md bg-amber-600/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-black/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-600 sm:text-sm/6" />
-              </div>
-            </div>
-
-            <div className='sm:col-span-full'>
-              <label htmlFor="message" className="block text-sm/6 font-semibold ">Message</label>
-              <div className='mt-2'>
-                <textarea id='name' placeholder='Enter your message here..' className="block w-full rounded-md bg-amber-600/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-black/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-600 sm:text-sm/6" />
-              </div>
-            </div>
-          </div>
-
-        </form>
+        <GoogleMap />
       </div>
     </section>
   )
